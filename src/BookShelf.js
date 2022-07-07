96f9f89e84f4f9feb93cde7eb1a6dc20d5897680
bhupesh Joshi <bhupeshait@gmail.com>
2022-07-07 11:16:34 +0530

    changed BookShelf component to a functional component

--- src/BookShelf.js
+++ src/BookShelf.js
@@ -1,6 +1,24 @@
 import React from "react";
 import Book from "./Book";
 
+function BookShelf(props) {
+  return (
+    <div className="bookshelf">
+      <h2 className="bookshelf-title">{props.readStatus}</h2>
+      <div className="bookshelf-books">
+        <ol className="books-grid">
+          {props.books.map((x) => (
+            <li key={x.id}>
+              <Book book={x} handleChange={props.handleChange} />
+            </li>
+          ))}
+        </ol>
+      </div>
+    </div>
+  );
+}
+
+/*
 class BookShelf extends React.Component {
   render() {
     return (
@@ -19,5 +37,5 @@ class BookShelf extends React.Component {
     );
   }
 }
-
+*/
 export default BookShelf;
